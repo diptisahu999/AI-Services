280cd08e96910f419d19b0a7dd243c73d67fce10
Diptiranjan <diptiranjansahu999@gmail.com>
2026-03-19 12:45:33 +0530

    add Home page UI design

--- app/routers/web.py
+++ app/routers/web.py
@@ -9,3 +9,13 @@ templates = Jinja2Templates(directory='app/templates')
 @router.get('/', response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
+
+
+@router.get('/image-cleaner', response_class=HTMLResponse)
+async def cleaner_page(request: Request):
+    return templates.TemplateResponse('cleaner.html', {'request': request})
+
+
+@router.get('/image-to-art', response_class=HTMLResponse)
+async def art_page(request: Request):
+    return templates.TemplateResponse('art.html', {'request': request})
